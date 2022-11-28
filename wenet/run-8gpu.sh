465b1ca2820b8172891279b9427fc7961fdbfd05
goriri <goriri@163.com>
2022-11-28 04:46:02 +0000

    fix

--- wenet/run-8gpu.sh
+++ wenet/run-8gpu.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-pip install -r requirements.txt
-# pip install torchaudio==0.10.0
-# echo "torchaudio installed"
-# conda install pytorch=1.10.0 torchvision torchaudio=0.10.0 cudatoolkit=11.1 -c pytorch -c conda-forge
+if [ ${stop_stage} -ge 4 ]; then
+  pip install -r requirements.txt
+fi
 
 # Copyright 2019 Mobvoi Inc. All Rights Reserved.
 
